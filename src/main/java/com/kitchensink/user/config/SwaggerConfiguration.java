56a460bb4efb698d19dace8d756846365de6c88b
Manmeet Devgun <manmeet.devgun@nagarro.com>
2025-05-01 04:07:35 +0530

    add endpoint security

--- src/main/java/com/kitchensink/user/config/SwaggerConfiguration.java
+++ src/main/java/com/kitchensink/user/config/SwaggerConfiguration.java
@@ -2,7 +2,12 @@ package com.kitchensink.user.config;
 
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 @Configuration
+@SecurityScheme(name = "Keycloak", openIdConnectUrl = "http://localhost:8080/realms/dive-dev/.well-known/openid-configuration", scheme = "bearer", type = SecuritySchemeType.OPENIDCONNECT, in = SecuritySchemeIn.HEADER)
 public class SwaggerConfiguration {
 
 }
--- src/main/java/com/kitchensink/user/controller/MemberController.java
+++ src/main/java/com/kitchensink/user/controller/MemberController.java
@@ -25,11 +25,13 @@ import com.kitchensink.user.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @Tag(name = "Member Management", description = "Operations related to member registration and retrieval")
+@SecurityRequirement(name = "Keycloak")
 public class MemberController {
 
 	@Autowired
