c542198a3c931b42a201361879fc215f99cecced
Manmeet Devgun <manmeet.devgun@nagarro.com>
2025-05-05 03:33:34 +0530

    update

--- src/main/java/com/kitchensink/user/KitchenSinkUserApplication.java
+++ src/main/java/com/kitchensink/user/KitchenSinkUserApplication.java
@@ -3,9 +3,11 @@ package com.kitchensink.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @Configuration
+@EnableMongoAuditing
 public class KitchenSinkUserApplication {
 
 	public static void main(String[] args) {
